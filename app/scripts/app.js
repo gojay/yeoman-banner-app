493ba8ff3300336ac7aacbef72cd0aba7825e9f7
Gojay <dani.gojay@gmail.com>
2014-06-25 22:19:15 +0700

    add template conversation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,41 +1,46 @@
 /*jshint unused: vars */
-define(['angular', 'controllers/main', 'controllers/bootstrap', 'controllers/banner']/*deps*/, function (angular, MainCtrl, BootstrapCtrl, BannerCtrl)/*invoke*/ {
-  'use strict';
+define(['angular', 'controllers/main', 'controllers/bootstrap', 'controllers/banner', 'controllers/conversation'] /*deps*/ , function(angular, MainCtrl, BootstrapCtrl, BannerCtrl, ConversationCtrl) /*invoke*/ {
+    'use strict';
 
-  return angular.module('bannerAppApp', ['bannerAppApp.controllers.MainCtrl',
-'bannerAppApp.controllers.BootstrapCtrl',
-'bannerAppApp.controllers.BannerCtrl',
-/*angJSDeps*/
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute',
+    return angular.module('bannerAppApp', ['bannerAppApp.controllers.MainCtrl',
+        'bannerAppApp.controllers.BootstrapCtrl',
+        'bannerAppApp.controllers.BannerCtrl',
+        'bannerAppApp.controllers.ConversationCtrl',
+        /*angJSDeps*/
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
 
-  'ngAnimate',
+        'ngAnimate',
 
-  'ui.bootstrap',
-  'ui.utils'
-])
-    .config(function ($routeProvider, $locationProvider) {
-      $routeProvider
-        .when('/', {
-          templateUrl: 'views/main.html',
-          controller: 'MainCtrl'
-        })
-        .when('/bootstrap', {
-          templateUrl: 'views/bootstrap.html',
-          controller: 'BootstrapCtrl'
-        })
-        .when('/facebook/banner', {
-          templateUrl: 'views/banner.html',
-          controller: 'BannerCtrl'
-        })
-        .otherwise({
-          redirectTo: '/'
+        'ui.bootstrap',
+        'ui.utils'
+    ])
+        .config(function($routeProvider, $locationProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'views/main.html',
+                    controller: 'MainCtrl'
+                })
+                .when('/bootstrap', {
+                    templateUrl: 'views/bootstrap.html',
+                    controller: 'BootstrapCtrl'
+                })
+                .when('/facebook/banner', {
+                    templateUrl: 'views/banner.html',
+                    controller: 'BannerCtrl'
+                })
+                .when('/facebook/conversation', {
+                    templateUrl: 'views/conversation.html',
+                    controller: 'ConversationCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+
+            // $locationProvider
+            //     .html5Mode(false)
+            //     .hashPrefix('!');
         });
-
-      // $locationProvider
-      //     .html5Mode(false)
-      //     .hashPrefix('!');
-    });
 });
